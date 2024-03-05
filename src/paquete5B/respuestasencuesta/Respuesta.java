1a38d4c6147088cbcac5173e86988dc2603963cf
RosioSantana <rosio.santana2242@uppuebla.edu.mx>
2024-03-04 18:43:36 -0600

    Clase Respuesta

---
+++ src/paquete5B/respuestasencuesta/Respuesta.java
@@ -0,0 +1,31 @@
+package paquete5B.respuestasencuesta;
+
+public class Respuesta {
+    private int matricula;
+    private int folio;
+    private String respuesta;
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public int getFolio() {
+        return folio;
+    }
+
+    public void setFolio(int folio) {
+        this.folio = folio;
+    }
+
+    public String getRespuesta() {
+        return respuesta;
+    }
+
+    public void setRespuesta(String respuesta) {
+        this.respuesta = respuesta;
+    }
+}
